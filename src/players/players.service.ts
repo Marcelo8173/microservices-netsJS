dc20b16cf65ed9cf3a7b619fc8434c903bfde716
Marcelo nascimento <marcelo.nascimento@skyone.solutions>
2021-08-21 17:35:04 -0300

    intall uuid lib

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { IPlayerDTO } from './DTO/player.dto';
 import { PlayerModel } from './player.model'
+import { v4 as uuid } from 'uuid'
 
 @Injectable()
 export class PlayersService {
     private playerArray = []
 
-    async createPlayerService(playerDTO: IPlayerDTO ): Promise<IPlayerDTO> {
-       //persistir
-        
+    async createPlayerService(playerDTO: IPlayerDTO ): Promise<IPlayerDTO> {        
         const player: PlayerModel = { 
-            id:"12312",
+            id:uuid(),
             name:playerDTO.name,
             email: playerDTO.email,
             phone_number: playerDTO.phone_number
